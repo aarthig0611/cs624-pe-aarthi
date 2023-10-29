eb105af10b0b4f5bce7f00a90d3c4b9829f69fc0
Aarthi Ganesan <aarthig0611@gmail.com>
2023-10-29 20:07:11 +0000

    Submission for Module03 - Aarthi Ganesan

---
+++ PE03-todos/PE03 README.md
@@ -0,0 +1,14 @@
+# Input
+
+The input section primarily deals with user interaction, where users can enter task descriptions, and the application captures this data for further processing. Users input their tasks via a text input field in the app's user interface.
+
+# Process
+
+The program processes the user input by managing the state of the application. It stores the tasks entered by the user, allowing them to view, modify, and delete these tasks. The core process involves the application's state management using React Native components and state variables. These components handle task creation, modification, and deletion. Additionally, the program likely includes logic for rendering the tasks on the screen and providing a responsive user interface.
+
+# Output
+
+In the output section, the program displays the tasks to the user. Users can see the list of tasks they've entered. The output consists of a list of tasks, dynamically updated as the user interacts with the application.
+
+
+In summary, the "Todo" app successfully adheres to the IPO model by taking input, processing it effectively, and generating a visually appealing and informative output for users.
--- PE03-todos/todos/App.js
+++ PE03-todos/todos/App.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import {View, ScrollView, StyleSheet} from 'react-native';
-import Heading from './Heading';
-import Input from './Input';
-import Button from './Button';
+import Heading from './app/Heading'
+import Input from './app/Input';
+import Button from './app/Button';
+import TodoList from './app/TodoList';
+import TabBar from './app/TabBar'
 
 let todoIndex = 0
 
@@ -15,6 +17,29 @@ class App extends Component {
       type: 'All',
     };
     this.submitTodo = this.submitTodo.bind(this)
+    this.toggleComplete = this.toggleComplete.bind(this)
+    this.deleteTodo = this.deleteTodo.bind(this)
+    this.setType = this.setType.bind(this)
+  }
+
+  deleteTodo (todoIndex) {
+    let { todos } = this.state
+    todos = todos.filter((todo) => todo.todoIndex !== todoIndex)
+    this.setState({ todos })
+  }
+
+  toggleComplete (todoIndex) {
+    let todos = this.state.todos
+    todos.forEach((todo) => {
+      if (todo.todoIndex === todoIndex) {
+        todo.complete = !todo.complete
+      }
+    })
+    this.setState({ todos })
+  }
+
+  setType (type) {
+    this.setState({ type })
   }
 
   inputChange(inputValue) {
@@ -39,7 +64,7 @@ class App extends Component {
   }
 
   render() {
-    const {inputValue} = this.state;
+    const {inputValue, todos, type } = this.state;
     return (
       <View style={styles.container}>
         <ScrollView keyboardShouldPersistTaps="always" style={styles.content}>
@@ -48,8 +73,14 @@ class App extends Component {
             inputValue={inputValue}
             inputChange={text => this.inputChange(text)}
           />
+          <TodoList 
+            type={type}
+            toggleComplete={this.toggleComplete}
+            deleteTodo={this.deleteTodo}
+            todos={todos} />
           <Button submitTodo={this.submitTodo}/>
         </ScrollView>
+        <TabBar type={type} setType={this.setType} />
       </View>
     );
   }
